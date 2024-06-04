49aecd862fedaf7e4b83e1111a31b44aee9f806e
showkatbinidris70 <showkatbinidris70@gmail.com>
2024-06-04 18:21:23 +0600

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const links = document.querySelectorAll(".nav-link");
+  const currentUrl = window.location.pathname.split("/").pop();
+
+  links.forEach((link) => {
+    const linkUrl = link.getAttribute("href");
+    if (
+      linkUrl === currentUrl ||
+      (linkUrl === "index.html" && currentUrl === "")
+    ) {
+      link.classList.add("active");
+    } else {
+      link.classList.remove("active");
+    }
+  });
+});
+
+// lightbox video start
+// Function to reveal lightbox and adding YouTube autoplay
+function revealVideo(div, video_id) {
+  var video = document.getElementById(video_id).src;
+  document.getElementById(video_id).src = video + "&autoplay=1"; // adding autoplay to the URL
+  document.getElementById(div).style.display = "block";
+}
+
+// Hiding the lightbox and removing YouTube autoplay
+function hideVideo(div, video_id) {
+  var video = document.getElementById(video_id).src;
+  var cleaned = video.replace("&autoplay=1", ""); // removing autoplay form url
+  document.getElementById(video_id).src = cleaned;
+  document.getElementById(div).style.display = "none";
+}
+// lightbox video end
