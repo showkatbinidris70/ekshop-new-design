679e5b69e8dec8131b3527ea084ab16b008b0400
showkatbinidris70 <showkatbinidris70@gmail.com>
2024-06-20 15:32:57 +0600

    slider

--- assets/js/main.js
+++ assets/js/main.js
@@ -49,3 +49,30 @@ function addPasswordShowHide(el) {
 }
 
 document.querySelectorAll(".show_hide_password").forEach(addPasswordShowHide);
+
+// swiper slider
+var swiper = new Swiper(".swiper", {
+  effect: "coverflow",
+  loop: true,
+  grabCursor: true,
+  centeredSlides: true,
+  initialSlide: 2,
+  speed: 600,
+  preventClicks: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 80,
+    depth: 350,
+    modifier: 1,
+    slideShadows: true,
+  },
+  on: {
+    click(event) {
+      swiper.slideTo(this.clickedIndex);
+    },
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
